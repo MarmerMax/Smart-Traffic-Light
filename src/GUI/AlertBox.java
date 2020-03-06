e1d4b038058bde9eab3e2a79cdf286ada6594967
Your Name <you@example.com>
2020-03-06 19:48:42 +0200

    create AlertBox, create AboutUs window

---
+++ src/GUI/AlertBox.java
@@ -0,0 +1,34 @@
+package GUI;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class AlertBox {
+
+    public static void display(String title, String message){
+        Stage window = new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+
+        Label label = new Label();
+        label.setText(message);
+        Button button = new Button("Close");
+        button.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(20);
+        layout.getChildren().addAll(label, button);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout, 250, 200);
+        window.setScene(scene);
+        window.setResizable(false);
+        window.showAndWait();
+    }
+
+}
--- src/GUI/ProgramGUI.java
+++ src/GUI/ProgramGUI.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -7,11 +8,12 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ProgramGUI {
 
-    private Scene scene1, scene2;
+    private Scene windowHome, windowOptions;
     private Stage window;
 
     public ProgramGUI(Stage stage){
@@ -20,36 +22,62 @@ public class ProgramGUI {
     }
 
     private void createUI(){
+        createHomeWindow();
         createOptionsWindow();
-        createSystemWindow();
+    }
+
+    private void createHomeWindow(){
+        Label label1 = new Label("Welcome to the home page!!!");
+        Button buttonStart = new Button("Let's start");
+        Button buttonAboutUs = new Button("About us");
+
+        buttonStart.setOnAction(e -> window.setScene(windowOptions));
+        buttonAboutUs.setOnAction(e -> createAboutUsAlertWindow());
+
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, buttonStart, buttonAboutUs);
+
+        windowHome = new Scene(layout1, 600, 400);
+    }
+
+    private void createAboutUsAlertWindow(){
+        Stage windowAboutUs = new Stage();
+
+        windowAboutUs.initModality(Modality.APPLICATION_MODAL);
+        windowAboutUs.setTitle("About us");
+
+        Label label = new Label();
+        label.setText("Smart Traffic Light\n" +
+                "Version 1.0\n" +
+                "Created by Netanel Davidov and Maxim Marmer");
+        Button button = new Button("Close");
+        button.setOnAction(e -> windowAboutUs.close());
+
+        VBox layout = new VBox(20);
+        layout.getChildren().addAll(label, button);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        windowAboutUs.setScene(scene);
+        windowAboutUs.setResizable(false);
+        windowAboutUs.showAndWait();
     }
 
     private void createOptionsWindow(){
-        Label label1 = new Label("Welcome to home page!!!");
-        Button button1 = new Button("Start system!");
-        button1.setOnAction(e -> window.setScene(scene2));
-
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1);
-        scene1 = new Scene(layout1, 200, 200);
-    }
-
-    private void createSystemWindow(){
-        Button button2 = new Button("Go home");
-        button2.setOnAction(e -> window.setScene(scene1));
+        Button buttonBackHome = new Button("Back home");
+        buttonBackHome.setOnAction(e -> window.setScene(windowHome));
 
         String imgPath = "file:images/lights/red-s.png";
         Image img = new Image(imgPath);
 
-
         StackPane layout2 = new StackPane();
-        layout2.getChildren().add(button2);
+        layout2.getChildren().add(buttonBackHome);
         layout2.getChildren().add(new ImageView(img));
-        scene2 = new Scene(layout2, 1300, 660);
+        windowOptions = new Scene(layout2, 1000, 660);
     }
 
     public Scene getScene(){
-        return scene1;
+        return windowHome;
     }
 
 }
