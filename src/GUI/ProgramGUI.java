c296af82b3779e5f95b39b08e60336caa3b2965c
MarmerMax <killlasta@gmail.com>
2020-03-07 06:39:11 +0200

    create client type window

--- src/GUI/ProgramGUI.java
+++ src/GUI/ProgramGUI.java
@@ -8,16 +8,13 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ProgramGUI {
 
-    private Scene windowHome, windowOptions;
+    private Scene windowHome, windowClientTypes, windowOptions;
     private Stage window;
+    private boolean analyst;
 
     public ProgramGUI(Stage stage) {
         window = stage;
@@ -27,10 +24,10 @@ public class ProgramGUI {
     private void createUI() {
         createHomeWindow();
         createOptionsWindow();
+        createClientTypesWindow();
     }
 
     private void createHomeWindow() {
-
         Label label = new Label("Welcome to the Smart traffic light!");
         HBox topMenu = new HBox();
         topMenu.setMinHeight(80);
@@ -38,7 +35,7 @@ public class ProgramGUI {
         topMenu.getChildren().addAll(label);
 
         Button buttonStart = new Button("Let's start");
-        buttonStart.setOnAction(e -> window.setScene(windowOptions));
+        buttonStart.setOnAction(e -> window.setScene(windowClientTypes));
 
         Button buttonAboutUs = new Button("About us");
         buttonAboutUs.setOnAction(e -> createAboutUsAlertWindow());
@@ -48,10 +45,9 @@ public class ProgramGUI {
         leftMenu.setSpacing(20);
         leftMenu.getChildren().addAll(buttonStart, buttonAboutUs);
 
-
         VBox rightMenu = new VBox();
-        Image image = new Image("file:images/others/main-image.jpg");
-        rightMenu.setPrefSize(400, 260);
+        Image image = new Image("file:images/others/main-image-3.jpg");
+        rightMenu.setPrefSize(400, 400);
         BackgroundImage backgroundimage = new BackgroundImage(image,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
@@ -77,6 +73,15 @@ public class ProgramGUI {
     }
 
     private void createOptionsWindow() {
+        Label label = new Label("Choose traffic situation");
+        HBox topMenu = new HBox();
+        topMenu.setMinHeight(80);
+        topMenu.setAlignment(Pos.CENTER);
+        topMenu.getChildren().addAll(label);
+
+        //
+        //
+
         Button buttonBackHome = new Button("Back home");
         buttonBackHome.setOnAction(e -> window.setScene(windowHome));
 
@@ -87,6 +92,47 @@ public class ProgramGUI {
         layout2.getChildren().add(buttonBackHome);
         layout2.getChildren().add(new ImageView(img));
         windowOptions = new Scene(layout2, 1000, 660);
+    }
+
+    private void createClientTypesWindow() {
+        Label label = new Label("Choose your occupation");
+        HBox topMenu = new HBox();
+        topMenu.setMinHeight(80);
+        topMenu.setAlignment(Pos.CENTER);
+        topMenu.getChildren().addAll(label);
+
+        VBox leftMenu = new VBox(20);
+        Button buttonAnalyst = new Button("Analyst");
+        buttonAnalyst.setOnAction(e -> {
+            analyst = true;
+            window.setScene(windowOptions);
+        });
+        leftMenu.setAlignment(Pos.CENTER);
+        Image imageAnalyst = new Image("file:images/others/analyst.png");
+        ImageView imageViewAnalyst = new ImageView(imageAnalyst);
+        imageViewAnalyst.setFitHeight(236);
+        imageViewAnalyst.setFitWidth(250);
+        leftMenu.getChildren().addAll(imageViewAnalyst, buttonAnalyst);
+
+        VBox rightMenu = new VBox(20);
+        Button buttonObserver = new Button("Observer");
+        buttonObserver.setOnAction(e -> {
+            analyst = false;
+            window.setScene(windowOptions);
+        });
+        rightMenu.setAlignment(Pos.CENTER);
+        Image imageObserver = new Image("file:images/others/observer.png");
+        ImageView imageViewObserver = new ImageView(imageObserver);
+        imageViewObserver.setFitHeight(236);
+        imageViewObserver.setFitWidth(250);
+        rightMenu.getChildren().addAll(imageViewObserver, buttonObserver);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.getStylesheets().add("file:src/GUI/style.css");
+        borderPane.setTop(topMenu);
+        borderPane.setLeft(leftMenu);
+        borderPane.setRight(rightMenu);
+        windowClientTypes = new Scene(borderPane, 600, 400);
     }
 
     public Scene getScene() {
