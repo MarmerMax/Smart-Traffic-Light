e2373d045c9bf40835d372d89581651c6f340898
Your Name <you@example.com>
2020-03-16 20:57:58 +0200

    system class changes

--- src/System/System.java
+++ src/System/System.java
@@ -1,7 +1,15 @@
 package System;
 
+import Objects.SystemConditions.Conditions;
+
 public class System {
 
+    private Conditions conditions;
 
+    public void setConditions(Conditions conditions) {
+        this.conditions = conditions;
+    }
 
+    public void run(){
+    }
 }
